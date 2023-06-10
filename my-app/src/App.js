d81e621cae617aac3ec6f1dea8b692fae61c47e2
Ryan Lewis <lewisworsley4@hotmail.co.uk>
2023-06-10 15:11:05 +0000

    Handle click method used to change state properties

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,19 @@
 import css from './App.module.css';
 import SideBar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+
+// function App() {
+//   return (
+//     <div className={css.App}>
+//       <SideBar />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className={css.App}>
-      <SideBar />
+    <div>
+      <StatefulGreeting greeting="no idea what I'm doing lol" name="Lewis"/>
     </div>
   );
 }
